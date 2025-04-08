9bb2c143bcdf4546b6d88f3c8f68889e56cc7f6a
bhendi-boi <bhendiisgreen@gmail.com>
2025-04-08 11:43:17 +0530

    updated `apb_pkg`

--- apb/apb_pkg.sv
+++ apb/apb_pkg.sv
@@ -5,5 +5,10 @@ package apb_pkg;
 
     import uvm_pkg::*;
     `include "apb_types.sv"
+    `include "apb_tr.sv"
+    `include "apb_sequencer.sv"
+    `include "apb_driver.sv"
+    `include "apb_monitor.sv"
+    `include "apb_agent.sv"
 
 endpackage
