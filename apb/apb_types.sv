32d7b88921c17c810c9f281de0e27c9bd2c6e73b
bhendi-boi <bhendiisgreen@gmail.com>
2025-04-08 11:39:46 +0530

    added new apb_types

--- apb/apb_types.sv
+++ apb/apb_types.sv
@@ -1,1 +1,9 @@
 `define APB_ADDR_WIDTH 12
+typedef bit [`APB_ADDR_WIDTH-1:0] apb_addr;
+typedef bit [31:0] apb_wdata;
+// since it is an output logic
+typedef logic [31:0] apb_rdata;
+typedef enum bit {
+    APB_READ  = 0,
+    APB_WRITE = 1
+} apb_dir;
